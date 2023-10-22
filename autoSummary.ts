30f7e0e37aa79b19b76789aab7f31712dda5adaf
Okysu <yby@ecanse.com>
2023-10-22 16:18:34 +0800

    feat: AISummary Supported.

--- autoSummary.ts
+++ autoSummary.ts
@@ -58,7 +58,7 @@ const generateAutoSummary = async (content: string): Promise<string> => {
   return result.choices[0].message.content;
 };
 
-export async function startAISummary() {
+export function startAISummary() {
   // get the current working directory
   var currentDir = process.cwd();
   var postsDir = path.join(currentDir, "pages/posts");
@@ -70,21 +70,24 @@ export async function startAISummary() {
   for (let i = 0; i < posts.length; i++) {
     const post = posts[i];
     const postContent = fs.readFileSync(path.join(postsDir, post), "utf-8");
-    const { data, content } = matter(postContent);
+    let { data, content } = matter(postContent);
     // if data.excerpt is not present and data.excerpt_type is ai then generate the excerpt
     const shouldGenerateExcerpt = !data.excerpt && data.excerpt_type === "ai";
 
     if (shouldGenerateExcerpt) {
       consola.log(`Generating excerpt for ${post}`);
       try {
-        const excerpt = await generateAutoSummary(content);
+        const excerpt = generateAutoSummary(content);
         consola.log(`Generated excerpt for ${post}`);
         // add the excerpt to the post data
-        data.excerpt = excerpt;
-        // add the excerpt to the post content
-        const newPostContent = matter.stringify(content, data);
-        // write the new post content to the file
-        fs.writeFileSync(path.join(postsDir, post), newPostContent);
+        excerpt.then((value) => {
+          data.excerpt = value;
+          content = "\n" + value + "\n<!-- more -->\n" + content;
+          // add the excerpt to the post content
+          const newPostContent = matter.stringify(content, data);
+          // write the new post content to the file
+          fs.writeFileSync(path.join(postsDir, post), newPostContent);
+        });
       } catch (error) {
         consola.error(`Error generating excerpt for ${post}`);
         consola.error(error);
@@ -92,4 +95,3 @@ export async function startAISummary() {
     }
   }
 }
-
